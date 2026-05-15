10784709c4c14f604f6ed1c0acff963a495b4ba7
iliya.smirnov <iliya.smirnov1@gmail.com>
2026-05-15 17:11:40 +0300

    adding supabase

---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink :to="{ name: '/projects/[id]', params: {'id': 123}}">Go to project 123</RouterLink>
+  </div>
+</template>
